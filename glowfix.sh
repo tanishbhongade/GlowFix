6fc91d70b99b729eee9b9a321ba8b7f4cd72e47f
Tanish Bhongade <tanishbhongade@gmail.com>
2024-10-04 23:31:36 +0530

    Fixed a small bug

--- glowfix.sh
+++ glowfix.sh
@@ -3,9 +3,7 @@
 # Watch the file for changes
 inotifywait -m -e modify /sys/class/backlight/*/brightness |
 while read path action file; do
-    # Run your script or command
-    # /usr/local/bin/my-brightness-adjuster/smart-brightness-adjuster
-    cd /usr/local/bin/my-brightness-adjuster
-    python /usr/local/bin/my-brightness-adjuster/main.py
+    cd /usr/local/bin/glowfix
+    python /usr/local/bin/glowfix/main.py
 done
 
